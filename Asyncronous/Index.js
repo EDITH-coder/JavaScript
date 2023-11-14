c5a6e6872e2469722d3f2d74decf1c830dbff984
EDITH_Coder <edithmashapo@gmail.com>
2023-11-14 12:32:39 +0200

    first commit

---
+++ Asyncronous/Index.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+function editBook(index) {
+    const newTitle = prompt('Enter new title:');
+    const newAuthor = prompt('Enter new author:');
+    const newImage = prompt('Enter new image URL:');
+
+    if (newTitle !== null && newAuthor !== null && newImage !== null) {
+        const library = getLibraryFromStorage();
+        library[index].title = newTitle;
+        library[index].author = newAuthor;
+        library[index].image = newImage;
+        localStorage.setItem('library', JSON.stringify(library));
+        displayBooks();
+    }
+}
